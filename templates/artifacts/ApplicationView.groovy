696bf4d2452584c494bb4ab156f4e6765b5a67d7
Dean Iverson <deanriverson@gmail.com>
2011-08-15 09:04:16 -0600

    Updated ApplicationView template with Text node changes.

--- templates/artifacts/ApplicationView.groovy
+++ templates/artifacts/ApplicationView.groovy
@@ -2,10 +2,10 @@
 application(title: '@griffon.app.class.name@') {
     scene(fill: "black") {
         hbox(padding: 100) {
-            text(content: "Java", font: "80pt sanserif") {
+            text(text: "Java", font: "80pt sanserif") {
                 linearGradient(endX: 0, stops: [[0, orange], [1, chocolate]])
             }
-            text(content: "FX", font: "80pt sanserif") {
+            text(text: "FX", font: "80pt sanserif") {
                 linearGradient(endX: 0, stops: [[0, cyan], [1, dodgerblue]])
                 dropShadow(color: dodgerblue, radius: 25, spread: 0.25)
             }

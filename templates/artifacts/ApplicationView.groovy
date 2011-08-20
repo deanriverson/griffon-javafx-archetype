9087b706fb0318e7adaea028cac7984333846fb1
Dean Iverson <deanriverson@gmail.com>
2011-08-19 19:33:43 -0600

    Updated templates for the latest GroovyFX

--- templates/artifacts/ApplicationView.groovy
+++ templates/artifacts/ApplicationView.groovy
@@ -1,13 +1,13 @@
 @artifact.package@
-application(title: '@griffon.app.class.name@') {
-    scene(fill: "black") {
-        hbox(padding: 100) {
+stage(title: '@griffon.app.class.name@', visible: true, centerOnScreen: true) {
+    scene(fill: black, width: 400, height: 300) {
+        hbox(padding: 80) {
             text(text: "Java", font: "80pt sanserif") {
-                linearGradient(endX: 0, stops: [[0, orange], [1, chocolate]])
+                fill linearGradient(endX: 0, stops: [[0, orange], [1, chocolate]])
             }
             text(text: "FX", font: "80pt sanserif") {
-                linearGradient(endX: 0, stops: [[0, cyan], [1, dodgerblue]])
-                dropShadow(color: dodgerblue, radius: 25, spread: 0.25)
+                fill linearGradient(endX: 0, stops: [[0, cyan], [1, dodgerblue]])
+                effect dropShadow(color: dodgerblue, radius: 25, spread: 0.25)
             }
         }
     }

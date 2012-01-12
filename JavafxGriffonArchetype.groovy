63fd0d12e257fc5f83cdffd91c910827564fc61d
aalmiray <aalmiray@gmail.com>
2012-01-12 18:28:12 +0100

    Update to 0.9.5

---
+++ JavafxGriffonArchetype.groovy
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Dean Iverson
+ * @author Andres Almiray
+ */
+class JavafxGriffonArchetype {
+    String version = '0.1'
+    String griffonVersion = '0.9.5-SNAPSHOT > *'
+    String license = 'Apache Software License 2.0'
+    List authors = [
+        [
+            name: 'Dean Iverson',
+            email: 'deanriverson@gmail.com'
+        ]
+    ]
+    String title = 'Griffon JavaFX Archetype'
+    String description = '''
+Brief description of [JavaFX][1].
+
+Usage
+----
+Simply specify the name of the archetype (*javafx*) when invoking the `create-app`
+command, like this
+
+    griffon create-app sample -archetype=javafx
+
+Configuration
+-------------
+There's no additional configuration required by this archetype.'
+
+[1]: http://javafx.com/
+'''
+
+    // URL to the archetype's documentation
+    String documentation = 'http://griffon.codehaus.org/Javafx+Archetype'
+}
--- application.groovy
+++ application.groovy
@@ -1,35 +1,54 @@
+/*
+ * Copyright 2011-2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Dean Iverson
+ * @author Andres Almiray
+ */
+ 
 import griffon.util.Metadata
 
-includeTargets << griffonScript("_GriffonPlugins")
-includeTargets << griffonScript("_GriffonInit")
-includeTargets << griffonScript("CreateMvc")
+includeTargets << griffonScript('CreateMvc')
 
 /**
  * Create the JavaFX builder configuration
  */
 def createBuilderConfig = {
     def builderConfigFile = new File("${basedir}/griffon-app/conf/Builder.groovy")
-    builderConfigFile.text = """\
+    builderConfigFile.append('''
         |root {
         |    'griffon.javafx.JavaFXApplicationBuilder' {
         |        view = '*'
         |    }
-        |}""".stripMargin()
+        |}'''.stripMargin())
 }
 
 target(name: 'createApplicationProject', description: 'Creates a new app project', prehook: null, posthook: null) {
     createProjectWithDefaults()
 
-    argsMap.view = 'ApplicationView'
-    argsMap.controller = 'ApplicationController'
-    argsMap.model = 'ApplicationModel'
-    createMVC()
-
-    createBuilderConfig()
-
     // Install the javafx plugin
     Metadata md = Metadata.getInstance(new File("${basedir}/application.properties"))
     installPluginExternal md, 'javafx'
+
+    argsMap.view       = 'ApplicationView'
+    argsMap.controller = 'ApplicationController'
+    argsMap.model      = 'ApplicationModel'
+    createMVC()
+
+    createBuilderConfig()
 }
 
 setDefaultTarget(createApplicationProject)
